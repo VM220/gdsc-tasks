86410ee435233ee83cd182ba12ad151e3bf51636
VM220 <vmurarka.22@gmail.com>
2024-04-12 14:02:22 +0530

    Final win

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import logging
 import flet as ft #importing the module
-
+from layout import create_appbar
+from settings import Settings
 from solitaire import Solitaire
 
 def main(page: ft.Page):
-    solitaire=Solitaire()
+    def on_new_game(settings):
+        page.controls.pop()
+        new_solitaire = Solitaire(settings, on_win)
+        page.add(new_solitaire)
+        page.update()
+
+    def on_win():
+        page.add(ft.AlertDialog(title=ft.Text("YOU WIN!"), open=True, on_dismiss=lambda e: page.controls.pop()))
+        print("You win")
+        page.update()
+    
+    settings = Settings()
+    create_appbar(page, settings, on_new_game)
+
+    solitaire=Solitaire(settings,on_win)
     page.add(solitaire)
 
 ft.app(target=main,assets_dir="images")
